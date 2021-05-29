355ca28b473f92be9a89fe36ef43d9b0240447bd
mage1711 <zizo17111998@gmail.com>
2021-05-29 21:54:57 +0200

    fix: socket fix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from app import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, host='143.198.126.177',cors_allowed_origins="*")
+    socketio.run(app, host='0.0.0.0',cors_allowed_origins="*")
